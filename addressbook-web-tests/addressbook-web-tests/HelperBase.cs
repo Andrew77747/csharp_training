9966f31fbec4876d2192d6d1c8f3f079c212b1a5
Andrew77747 <andrew-walker@yandex.ru>
2023-02-22 14:27:32 +0300

    HelperBase add primitive methods

--- addressbook-web-tests/addressbook-web-tests/HelperBase.cs
+++ addressbook-web-tests/addressbook-web-tests/HelperBase.cs
@@ -5,10 +5,59 @@ namespace WebAddressbookTests
     public class HelperBase
     {
         protected IWebDriver driver;
+        private bool acceptNextAlert = true;
 
         public HelperBase(IWebDriver driver)
         {
             this.driver = driver;
+        }
+
+        public bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        public string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
